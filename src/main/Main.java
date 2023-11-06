236f24ed83326560a9a39221332cf597c8edb889
fransoufil <fran.sf@uol.com.br>
2023-11-06 20:06:59 -0300

    Refatorando as anotacoes para mais clareza

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,12 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+    	
+    	//DESCRICAO DO PROBLEMA
+    	//Dado o seguinte HashMap, devera exibir:
+    	//I- A soma total por esporte
+    	//II- A media de cada esporte
+    	//III- O nome do esporte com a maior media
         HashMap<String, String> mapAtletas = new HashMap<>();
 
         mapAtletas.put("Lionel Messi", "Futebol,130");
@@ -17,36 +23,40 @@ public class Main {
         mapAtletas.put("Cassius Kley", "Boxe,110");
         mapAtletas.put("Jaqueline", "Volei,97");
 
-        // Criar um mapa para armazenar a soma das pontuações por esporte e a contagem de atletas por esporte
+        // 1. Criar um mapa para armazenar a soma das pontuações por esporte 
+        //e outro para a contagem de atletas por esporte
         Map<String, Integer> somaPorEsporte = new HashMap<>();
         Map<String, Integer> contagemPorEsporte = new HashMap();
 
-        //separando os valores de value em cada mapAtletas
+        //2. separando as partes do value de cada mapAtletas
         mapAtletas.forEach((nome, info) -> {
-        	//fazendo o split com o caracter padrao de separacao
+        	//2.1 fazendo o split com o caracter padrao de separacao
             String[] partes = info.split(",");
+            //2.2 testando se ha duas partes dentro da String value -> info
             if (partes.length == 2) {
             	
-            	//havendo entao 2 partes dentro de value, separadas por virgula, a posicao 0 seria o nome do esporte e a posicao 1 seria o valor
+            	//2.3 havendo entao 2 partes dentro de value, separadas por virgula, 
+            	//a posicao 0 seria o nome do esporte e a posicao 1 seria o valor
                 String esporte = partes[0];
-                int pontuacao = Integer.parseInt(partes[1]);//ha a necessidade de conversao pois esta em formato String
+              //2.4 ha a necessidade de conversao da segunda posicao pois esta em formato String
+                int pontuacao = Integer.parseInt(partes[1]);
 
-                // Atualizar a soma das pontuações por esporte
+                // 3. atualizar a soma das pontuações por esporte
                 somaPorEsporte.put(esporte, somaPorEsporte.getOrDefault(esporte, 0) + pontuacao);
 
-                // Atualizar a contagem de atletas por esporte
+                // 4. Atualizar a contagem de frequencia de cada esporte
                 contagemPorEsporte.put(esporte, contagemPorEsporte.getOrDefault(esporte, 0) + 1);
             }
         });
         
-        // Exibir a soma total por esporte
+        //I- Exibir a soma total por esporte
         System.out.println("Soma total de pontuações por esporte:");
         for (Map.Entry<String, Integer> entry : somaPorEsporte.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
         
 
-        // Calcular a média de pontuações por esporte
+        //5. Calcular a média de pontuações por esporte
         Map<String, Double> mediaPorEsporte = new HashMap<>();
         for (String esporte : somaPorEsporte.keySet()) {
             int soma = somaPorEsporte.get(esporte);
@@ -55,19 +65,19 @@ public class Main {
             mediaPorEsporte.put(esporte, media);
         }
 
-        // Encontrar o esporte com a maior média
+        //6.  Encontrar o esporte com a maior média
         String esporteMaiorMedia = mediaPorEsporte.entrySet().stream()
                 .max(Map.Entry.comparingByValue())
                 .map(Map.Entry::getKey)
                 .orElse("Nenhum esporte encontrado");
         
-        // Exibir a média por esporte
+        //II- Exibir a média por esporte
         System.out.println("Média por esporte:");
         for (Map.Entry<String, Double> entry : mediaPorEsporte.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
 
-        // Exibir o esporte com a maior média
+        //III- Exibir o esporte com a maior média
         System.out.println("Esporte com a maior média: " + esporteMaiorMedia);
     }
 }
